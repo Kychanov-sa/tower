367ddb3e4144a21c6b07088c557262c83d14ba8b
kychanov-sa <kychanov.sa@gmail.com>
2019-05-18 18:56:20 +0400

    Annoncement

--- Tower/ViewModels/Annoncement.cs
+++ Tower/ViewModels/Annoncement.cs
@@ -1,4 +1,7 @@
-﻿namespace Tower.ViewModels
+﻿using System;
+using System.Windows;
+
+namespace Tower.ViewModels
 {
   public class Annoncement : BaseViewModel
   {
@@ -9,11 +12,42 @@
     }
     private string _title;
 
+    public DateTime PublishDate
+    {
+      get { return _publishDate; }
+      set { SetField(ref _publishDate, value); }
+    }
+    private DateTime _publishDate;
+
     public string Text
     {
       get { return _text; }
       set { SetField(ref _text, value); }
     }
     private string _text;
+
+    public bool IsRead
+    {
+      get { return _isRead; }
+      set { SetField(ref _isRead, value); OnPropertyChanged("TitleWeight"); }
+    }
+    private bool _isRead;
+
+    public bool IsImportant
+    {
+      get { return _isImportant; }
+      set { SetField(ref _isImportant, value); OnPropertyChanged("FlagVisibility"); }
+    }
+    private bool _isImportant;
+
+    public FontWeight TitleWeight
+    {
+      get { return (IsRead ? FontWeights.Regular : FontWeights.Bold); }
+    }
+
+    public Visibility FlagVisibility
+    {
+      get { return IsImportant ? Visibility.Visible : Visibility.Hidden;  }
+    }
   }
 }
--- Tower/ViewModels/MainWindow.cs
+++ Tower/ViewModels/MainWindow.cs
@@ -7,29 +7,47 @@ using System.Threading.Tasks;
 
 namespace Tower.ViewModels
 {
-  public class MainWindow
+  public class MainWindow : BaseViewModel
   {
-    public ObservableCollection<Annoncement> Annoncement { get; set; } = new ObservableCollection<ViewModels.Annoncement>();
+    public ObservableCollection<Annoncement> Annoncements { get; set; } = new ObservableCollection<ViewModels.Annoncement>();
+
+    public IEnumerable<Annoncement> UnreadAnnoncements { get { return from a in Annoncements where !a.IsRead select a; } }
+    public IEnumerable<Annoncement> ImportantAnnoncements { get { return from a in Annoncements where a.IsImportant select a; } }
 
     public MainWindow()
     {
-      Annoncement.Add(new ViewModels.Annoncement()
+      Annoncements.Add(new ViewModels.Annoncement()
       {
         Title = "Снова собрание жильцов",
-        Text = "Уважаемые жильцы! 32 ноября 2024г. в помещении подвала по адресу ул.Ленина, д.16 состоится собрание жильцов нашего дома. На собрании будут обсуждаться организационные вопросы создания в нашем доме Товарищества собственников жилья. В проведении собрания примет участие представитель ООО 'Безругие Бобры'. На собрание приглашаются все желающие. Инициатор проведения собрания Захапугин И.В."
+        Text = "Уважаемые жильцы! 32 ноября 2024г. в помещении подвала по адресу ул.Ленина, д.16 состоится собрание жильцов нашего дома. На собрании будут обсуждаться организационные вопросы создания в нашем доме Товарищества собственников жилья. В проведении собрания примет участие представитель ООО 'Безругие Бобры'. На собрание приглашаются все желающие. Инициатор проведения собрания Захапугин И.В.",
+        IsRead = false,
+        PublishDate = DateTime.Now.AddDays(-3),
+        IsImportant = true
       });
 
-      Annoncement.Add(new ViewModels.Annoncement()
+      Annoncements.Add(new ViewModels.Annoncement()
       {
         Title = "Не муссорить",
-        Text = "Уважаемые жильцы! Убедительная просьба не выкидывать мусор из окон квартир. С уважением, администрация этого зоопарка."
+        Text = "Уважаемые жильцы! Убедительная просьба не выкидывать мусор из окон квартир. С уважением, администрация этого зоопарка.",
+        IsRead = false,
+        PublishDate = DateTime.Now.AddDays(-2).AddHours(3),
+        IsImportant = false
       });
 
-      Annoncement.Add(new ViewModels.Annoncement()
+      Annoncements.Add(new ViewModels.Annoncement()
       {
         Title = "Продам таксу",
-        Text = "Продам таксу, можно по частям. Самомвывоз. т.32-54-23"
+        Text = "Продам таксу, можно по частям. Самомвывоз. т.32-54-23",
+        IsRead = true,
+        PublishDate = DateTime.Now.AddDays(-4).AddHours(-1).AddMinutes(-32),
+        IsImportant = false
       });
+    }
+
+    public void ReadAnnoncement(Annoncement annoncement)
+    {
+      annoncement.IsRead = true;
+      OnPropertyChanged("UnreadAnnoncements");
     }
   }
 }
